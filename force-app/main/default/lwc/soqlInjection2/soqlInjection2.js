8a91b1121bbac7bd732fcbeecd19d4ce1ca4952a
Alba Rivas <arivas@salesforce.com>
2021-09-15 11:25:19 +0200

    More injection examples

---
+++ force-app/main/default/lwc/soqlInjection2/soqlInjection2.js
@@ -0,0 +1,26 @@
+import { LightningElement, wire } from "lwc";
+import { reduceErrors } from "c/ldsUtils";
+import NAME_FIELD from "@salesforce/schema/Account.Name";
+import INDUSTRY_FIELD from "@salesforce/schema/Account.Industry";
+import getFilteredAccountsInjection from "@salesforce/apex/AccountController.getFilteredAccountsInjection2";
+
+const COLUMNS = [
+  { label: "Account Name", fieldName: NAME_FIELD.fieldApiName, type: "text" },
+  { label: "Industry", fieldName: INDUSTRY_FIELD.fieldApiName, type: "text" }
+];
+
+export default class SoqlInjection2 extends LightningElement {
+  columns = COLUMNS;
+  searchValue = "";
+
+  @wire(getFilteredAccountsInjection, { searchValue: "$searchValue" })
+  accounts;
+
+  get errors() {
+    return this.accounts.error ? reduceErrors(this.accounts.error) : [];
+  }
+
+  handleInputChange(event) {
+    this.searchValue = event.target.value;
+  }
+}
