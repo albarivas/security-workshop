273c2ada61b9ab0a2c13e89c0becd22ccc9722ac
Alba Rivas <arivas@salesforce.com>
2021-09-16 12:06:49 +0200

    Add exercise 2

---
+++ force-app/main/default/lwc/exercise2/exercise2.js
@@ -0,0 +1,19 @@
+import { LightningElement } from "lwc";
+
+export default class Exercise2 extends LightningElement {
+  numbers;
+  summary;
+
+  handleInputChange(event) {
+    this.numbers = event.detail.value;
+    const expr = '[' + this.numbers + ']'; 
+    const array = eval(expr);
+
+    let summary = 'These are the numbers: ';
+    array.forEach(number => {
+      summary += number + ' ';
+    });
+
+    this.summary = summary;
+  }
+}
--- force-app/main/default/lwc/reflectedXss/reflectedXss.js
+++ force-app/main/default/lwc/reflectedXss/reflectedXss.js
@@ -1,6 +1,4 @@
 import { LightningElement } from "lwc";
-import getContactXSS from "@salesforce/apex/ContactController.getContactXSS";
-
 export default class ReflectedXss extends LightningElement {
   handleInputChange(event) {
     eval(event.detail.value);
